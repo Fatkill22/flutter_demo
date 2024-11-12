747f40569f17bd78510681760458963aed9dc836
Ptrckjs <patrickmjose22@gmail.com>
2024-11-12 22:09:41 +0800

    initial commit

---
+++ android/app/src/main/kotlin/com/example/md_ea2_num1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.md_ea2_num1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Myapp()
+  );
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor : Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+            color: Colors.red,
+            width: 100,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              Container(
+                color: Colors.yellow,
+                height: 100,
+                width: 100,
+              ),
+              Container(
+                color: Colors.green,
+                height: 100,
+                width: 100,
+              ),
+            ]
+              ),
+              Container(
+                color: Colors.blue,
+                width: 100
+              )
+
+            ],
+          )
+        )
+      )
+    );
+  }
+}
+
+
